30d7a11c5867642df7c44d2f6cacd17c06fd0b63
Gwenhaell <97152351+Gwenhaell@users.noreply.github.com>
2022-11-28 19:31:50 +0800

    Update versionCode

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+versionCode=$(cat module.prop | grep 'versionCode=' | awk -F '=' '{print $2}')
+let versionCode++
+sed -i "s/versionCode=[0-9]*/versionCode=$versionCode/g" module.prop
+sed -i "s/\"versionCode\": [0-9]*/\"versionCode\": $versionCode/g" update.json
+
 zip -r -o -X -ll core_update.zip ./ -x '.git/*' -x 'build.sh' -x '.github/*' -x 'update.json'
